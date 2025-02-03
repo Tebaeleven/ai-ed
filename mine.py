a30133a07a812a048109d15a2faa98a13a9b6efe
Tebaeleven <73474645+Tebaeleven@users.noreply.github.com>
2025-02-03 17:41:19 +0900

    出力先の重みも参照できる用に

--- mine.py
+++ mine.py
@@ -17,7 +17,7 @@ def sigmoid_derivative(x):
 
 
 class Neuron:
-    def __init__(self,input_neurons: list["Neuron"],output_neurons: list["Neuron"], type: Literal["p", "n"]) -> None:
+    def __init__(self, input_neurons: list["Neuron"], output_neurons: list["Neuron"], type: Literal["p", "n"]) -> None:
         self.type = type
         self.activation = sigmoid
         # 相手から繋がってきているニューロン
@@ -27,6 +27,7 @@ class Neuron:
         self.output_neurons = output_neurons
 
         self.input_weights = []
+
         # --- init weights
         # 0～1の乱数で初期化                                                                                                                          
         # 接続の種類で符号を変える: pp+ pn- np- nn+
@@ -43,8 +44,12 @@ class Neuron:
                     ope = 1
             self.input_weights.append(0.2 * ope)
 
-        self.output_weights = [n.input_weights for n in output_neurons]
+        # output_weightsの初期化を後に移動
+        self.output_weights = []
 
+    def set_output_weights(self):
+        # output_weightsをoutput_neuronsに基づいて初期化
+        self.output_weights = [0.2 for _ in self.output_neurons]
 
     def forward(self, x):
         return sigmoid(x)
@@ -84,24 +89,47 @@ class ThreeLayerModel:
         # output
         self.out_neuron = Neuron([hd_p, hd_n] + self.hidden_neurons, [], "p")
 
+        # それぞれのニューロンに接続先の設定をする
+        for neuron in self.inputs:
+            neuron.output_neurons.append(self.hidden_neurons[0])
+            neuron.output_neurons.append(self.hidden_neurons[1])
+
+        for neuron in self.hidden_neurons:
+            neuron.output_neurons.append(self.out_neuron)
+
+        # output_weightsを設定
+        for neuron in self.inputs + self.hidden_neurons + [self.out_neuron]:
+            neuron.set_output_weights()
+
     def display_neuron_parameters(self):
         for neuron in self.inputs:
+            print("inputs")
+
             print(f"Neuron Type: {neuron.type}")
             print(f"Input Weights: {neuron.input_weights}")
             print(f"Output Weights: {neuron.output_weights}")
+            print(neuron.type)
+
             print("-" * 30)
     
         for neuron in self.hidden_neurons:
+            print("hidden")
+
             print(f"Neuron Type: {neuron.type}")
             print(f"Input Weights: {neuron.input_weights}")
             print(f"Output Weights: {neuron.output_weights}")
+            print(neuron.type)
+
             print("-" * 30)
 
+
         neuron = self.out_neuron
+        print("output")
         print(f"Neuron Type: {neuron.type}")
         print(f"Input Weights: {neuron.input_weights}")
         print(f"Output Weights: {neuron.output_weights}")
         print("-" * 30)
+
 
 
 def main():
